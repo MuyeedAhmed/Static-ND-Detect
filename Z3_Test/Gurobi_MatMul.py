07cd44b2368efa41a0b8942b611805e554b695a2
Muyeed Ahmed <akib100095@gmail.com>
2026-04-10 08:38:33 -0400

    Gurobi MatMul test

--- Z3_Test/Gurobi_MatMul.py
+++ Z3_Test/Gurobi_MatMul.py
@@ -4,6 +4,9 @@ import numpy as np
 import time
 import pickle
 
+import os
+from decimal import Decimal, getcontext
+
 def mat_mul_nxn_gurobi(model, A, B, n):
     C = [[model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY, name=f"C_{i}_{j}") for j in range(n)] for i in range(n)]
     
@@ -66,5 +69,42 @@ def check_matrix_associativity_gurobi(n=2):
     else:
         print("No counterexample.")
 
+
+
+getcontext().prec = 100
+
+def verify():
+    pickle_file = 'solution.pkl'
+    with open(pickle_file, 'rb') as f:
+        data = pickle.load(f)
+
+    A_list = data['A']
+    B_list = data['B']
+    C_list = data['C']
+
+    A_np = np.array(A_list)
+    B_np = np.array(B_list)
+    C_np = np.array(C_list)
+
+    print(f"A:\n{A_np}")
+    print(f"B:\n{B_np}")
+    print(f"C:\n{C_np}")
+
+    AB_np = np.dot(A_np, B_np)
+    ABC_np = np.dot(AB_np, C_np)
+    
+    BC_np = np.dot(B_np, C_np)
+    A_BC_np = np.dot(A_np, BC_np)
+    
+    print(f"(AB)C[0,0]: {ABC_np[0,0]}")
+    print(f"A(BC)[0,0]: {A_BC_np[0,0]}")
+    print(f"(AB)C:\n{ABC_np}")
+    print(f"A(BC):\n{A_BC_np}")
+
+    diff_np = ABC_np[0,0] - A_BC_np[0,0]
+    print(f"Difference: {diff_np}")
+    print(f"Squared Difference: {diff_np**2}")
+
 if __name__ == "__main__":
-    check_matrix_associativity_gurobi(2)
+    check_matrix_associativity_gurobi(3)
+    verify()
